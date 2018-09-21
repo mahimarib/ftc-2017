9a4ecff85b95c9d43be272aa947486878701e794
Mahim <mahim216@gmail.com>
2018-09-21 03:53:33 -0400

    switch to robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/ParkingLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/autonomous/ParkingLeft.java
@@ -3,32 +3,32 @@ package org.firstinspires.ftc.teamcode.commands.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.systems.MecanumDriveSystem;
+import org.firstinspires.ftc.teamcode.systems.Robot;
 
 /**
- * Created by nycfirst on 2/2/18.
+ * Created by Mahim on 2/2/18.
  */
-
 @Autonomous(name = "parking left", group = "parking")
 public class ParkingLeft extends LinearOpMode {
-    private MecanumDriveSystem mecanumDriveSystem;
+    private Robot robot = new Robot(this);
     private ElapsedTime runtime = new ElapsedTime();
 
     private void initialize() {
-        this.mecanumDriveSystem = new MecanumDriveSystem(hardwareMap);
+        robot.init(hardwareMap);
     }
 
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
         waitForStart();
+        runtime.reset();
         int count = 0;
+
         while (opModeIsActive() && (count < 1)) {
-            mecanumDriveSystem.drive(0.5, 0.5);
+            robot.driveSystem.drive(0.5, 0.5);
             sleep(2000);
             count++;
         }
-        mecanumDriveSystem.stop();
+        robot.driveSystem.stop();
     }
 }
