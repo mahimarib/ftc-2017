3e8c29d5d70f9016b127135014272a88ab1e24ae
Mahim <mahim216@gmail.com>
2018-11-03 18:04:43 -0400

    added javadoc to test opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/testcommands/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/testcommands/TurnTest.java
@@ -5,6 +5,9 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Robot;
 
+/**
+ * This opMode is used to test the turn command of the drivetrain.
+ */
 @Autonomous(name = "turn test", group = "test")
 public class TurnTest extends LinearOpMode {
     private Robot robot = new Robot(this);
