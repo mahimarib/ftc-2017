90b0a14302c1a5ca09501b154d5174e0006ae8a8
Mahim <mahim216@gmail.com>
2018-11-05 18:43:13 -0500

    created existing routines as CustomOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/testcommands/BlueTop.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.commands.testcommands;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.commands.CustomOpMode;
+import org.firstinspires.ftc.teamcode.systems.Drivetrain;
+
+@Autonomous(name = "new blue alliance top", group = "testing")
+public class BlueTop extends CustomOpMode {
+    @Override
+    public void initialize() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void execute() {
+        robot.knockDownRedJewel();
+        robot.drivetrain.stop();
+        sleep(1000);
+        robot.drivetrain.drive(0.5, 0.3, Drivetrain.Direction.FORWARD);
+        sleep(1000);
+        robot.drivetrain.stop();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/testcommands/RedTop.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.commands.testcommands;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.teamcode.commands.CustomOpMode;
+import org.firstinspires.ftc.teamcode.systems.Drivetrain;
+
+@Autonomous(name = "new red alliance top", group = "testing")
+public class RedTop extends CustomOpMode {
+    @Override
+    public void initialize() {
+        robot.init(hardwareMap);
+    }
+
+    @Override
+    public void execute() {
+        robot.knockDownBlueJewel();
+        robot.drivetrain.stop();
+        sleep(1000);
+        robot.drivetrain.drive(0.3, 0.5, Drivetrain.Direction.FORWARD);
+        sleep(1000);
+        robot.drivetrain.stop();
+    }
+}
