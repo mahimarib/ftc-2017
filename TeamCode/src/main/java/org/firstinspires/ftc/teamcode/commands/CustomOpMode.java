80c47eb0cb015ca6eee1b91174dbcc292573682d
Mahim <mahim216@gmail.com>
2018-11-05 18:42:31 -0500

    created CustomOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/CustomOpMode.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.Robot;
+
+/**
+ * CustomOpMode is an abstract class that all autonomous routines. It
+ * contains methods and/or instance variables that is common for all
+ * autonomous routines.
+ */
+public abstract class CustomOpMode extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    protected Robot robot = new Robot(this);
+
+    /**
+     * Runs actions that are needed before the match starts. Such as
+     * initializing the robot.
+     */
+    public abstract void initialize();
+
+    /**
+     * Runs actions that are needed during the match.
+     */
+    public abstract void execute();
+
+    @Override
+    public void runOpMode() {
+        initialize();
+        waitForStart();
+        runtime.reset();
+        execute();
+    }
+}
